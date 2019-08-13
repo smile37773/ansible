47cfe4d17527909a7e6e8d1dfa34c04d1c4c4862
smile37773 <714389288@qq.com>
2019-08-13 15:56:38 +0800

    add info modules

--- lib/ansible/modules/cloud/azure/apimanagementservice_info.py
+++ lib/ansible/modules/cloud/azure/apimanagementservice_info.py
@@ -561,11 +561,11 @@ class AzureRMApiManagementServiceInfo(AzureRMModuleBase):
 
         if (self.resource_group is not None and
             self.name is not None):
-            self.results['api_management_service'] = self.format_item(self.get())
+            self.results['api_management_service'] = self.get()
         elif (self.resource_group is not None):
-            self.results['api_management_service'] = self.format_item(self.listbyresourcegroup())
+            self.results['api_management_service'] = self.listbyresourcegroup()
         else:
-            self.results['api_management_service'] = [self.format_item(self.list())]
+            self.results['api_management_service'] = self.list()
         return self.results
 
     def get(self):
