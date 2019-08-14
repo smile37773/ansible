f51b3096e979ec6159af8041307c8dcbbfe4f49f
smile37773 <714389288@qq.com>
2019-08-14 17:35:40 +0800

    add modules

--- lib/ansible/modules/cloud/azure/apimanagementcertificate_info.py
+++ lib/ansible/modules/cloud/azure/apimanagementcertificate_info.py
@@ -76,11 +76,11 @@ author:
 
 EXAMPLES = '''
 - name: ApiManagementListCertificates
-  azure.rm.apimanagementcertificate.info:
+  azure.rm.apimanagementcertificate_info:
     resource_group: myResourceGroup
     service_name: myService
 - name: ApiManagementGetCertificate
-  azure.rm.apimanagementcertificate.info:
+  azure.rm.apimanagementcertificate_info:
     resource_group: myResourceGroup
     service_name: myService
     certificate_id: myCertificate
@@ -131,7 +131,11 @@ import json
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 from ansible.module_utils.azure_rm_common_rest import GenericRestClient
 from copy import deepcopy
-from msrestazure.azure_exceptions import CloudError
+try:
+  from msrestazure.azure_exceptions import CloudError
+except ImportError:
+  # This is handled in azure_rm_common
+  pass
 
 
 class AzureRMCertificateInfo(AzureRMModuleBase):
@@ -139,11 +143,11 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
         self.module_arg_spec = dict(
             resource_group=dict(
                 type='str',
-                required=true
+                required=True
             ),
             service_name=dict(
                 type='str',
-                required=true
+                required=True
             ),
             certificate_id=dict(
                 type='str'
@@ -153,10 +157,6 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
         self.resource_group = None
         self.service_name = None
         self.certificate_id = None
-        self.id = None
-        self.name = None
-        self.type = None
-        self.properties = None
 
         self.results = dict(changed=False)
         self.mgmt_client = None
@@ -183,10 +183,10 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
         if (self.resource_group is not None and
             self.service_name is not None and
             self.certificate_id is not None):
-            self.results['certificate'] = self.format_item(self.get())
+            self.results['certificate'] = self.get()
         elif (self.resource_group is not None and
               self.service_name is not None):
-            self.results['certificate'] = self.format_item(self.listbyservice())
+            self.results['certificate'] = self.listbyservice()
         return self.results
 
     def get(self):
@@ -206,7 +206,7 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ certificate_name }}', self.name)
+        self.url = self.url.replace('{{ certificate_name }}', self.certificate_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -217,12 +217,12 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return self.format_item(results)
 
     def listbyservice(self):
         response = None
@@ -251,15 +251,22 @@ class AzureRMCertificateInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return [self.format_item(x) for x in results['value']] if results['value'] else []
 
-    def format_item(item):
-        return item
+    def format_item(self, item):
+        d = {
+            'id': item['id'],
+            'name': item['name'],
+            'type': item['type'],
+            'data': item['properties']['data'],
+            'password': item['properties']['password']
+        }
+        return d
 
 
 def main():
--- lib/ansible/modules/cloud/azure/apimanagementtag_info.py
+++ lib/ansible/modules/cloud/azure/apimanagementtag_info.py
@@ -86,44 +86,44 @@ author:
 
 EXAMPLES = '''
 - name: ApiManagementListTags
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
 - name: ApiManagementGetTag
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     tag_id: myTag
 - name: ApiManagementListApiTags
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     api_id: myApi
 - name: ApiManagementGetApiTag
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     tag_id: myTag
     api_id: myApi
 - name: ApiManagementListProductTags
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     product_id: myProduct
 - name: ApiManagementGetProductTag
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     tag_id: myTag
     product_id: myProduct
 - name: ApiManagementListApiOperationTags
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     api_id: myApi
     operation_id: myOperation
 - name: ApiManagementGetApiOperationTag
-  azure.rm.apimanagementtag.info:
+  azure.rm.apimanagementtag_info:
     resource_group: myResourceGroup
     service_name: myService
     tag_id: myTag
@@ -176,7 +176,11 @@ import json
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 from ansible.module_utils.azure_rm_common_rest import GenericRestClient
 from copy import deepcopy
-from msrestazure.azure_exceptions import CloudError
+try:
+  from msrestazure.azure_exceptions import CloudError
+except ImportError:
+  # This is handled in azure_rm_common
+  pass
 
 
 class AzureRMTagInfo(AzureRMModuleBase):
@@ -184,11 +188,11 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.module_arg_spec = dict(
             resource_group=dict(
                 type='str',
-                required=true
+                required=True
             ),
             service_name=dict(
                 type='str',
-                required=true
+                required=True
             ),
             scope=dict(
                 type='str'
@@ -214,10 +218,6 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.api_id = None
         self.product_id = None
         self.operation_id = None
-        self.id = None
-        self.name = None
-        self.type = None
-        self.properties = None
 
         self.results = dict(changed=False)
         self.mgmt_client = None
@@ -246,37 +246,37 @@ class AzureRMTagInfo(AzureRMModuleBase):
             self.api_id is not None and
             self.operation_id is not None and
             self.tag_id is not None):
-            self.results['tag'] = self.format_item(self.getbyoperation())
+            self.results['tag'] = self.getbyoperation()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.api_id is not None and
               self.operation_id is not None):
-            self.results['tag'] = self.format_item(self.listbyoperation())
+            self.results['tag'] = self.listbyoperation()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.product_id is not None and
               self.tag_id is not None):
-            self.results['tag'] = self.format_item(self.getbyproduct())
+            self.results['tag'] = self.getbyproduct()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.api_id is not None and
               self.tag_id is not None):
-            self.results['tag'] = self.format_item(self.getbyapi())
+            self.results['tag'] = self.getbyapi()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.product_id is not None):
-            self.results['tag'] = self.format_item(self.listbyproduct())
+            self.results['tag'] = self.listbyproduct()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.api_id is not None):
-            self.results['tag'] = self.format_item(self.listbyapi())
+            self.results['tag'] = self.listbyapi()
         elif (self.resource_group is not None and
               self.service_name is not None and
               self.tag_id is not None):
-            self.results['tag'] = self.format_item(self.get())
+            self.results['tag'] = self.get()
         elif (self.resource_group is not None and
               self.service_name is not None):
-            self.results['tag'] = self.format_item(self.listbyservice())
+            self.results['tag'] = self.listbyservice()
         return self.results
 
     def getbyoperation(self):
@@ -300,9 +300,9 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ operation_name }}', self.operation_id)
+        self.url = self.url.replace('{{ tag_name }}', self.tag_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -313,12 +313,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return self.format_item(results)
 
     def listbyoperation(self):
         response = None
@@ -340,9 +340,8 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ operation_name }}', self.operation_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -353,12 +352,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return [self.format_item(x) for x in results['value']] if results['value'] else []
 
     def getbyproduct(self):
         response = None
@@ -379,9 +378,9 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ operation_name }}', self.product_id)
+        self.url = self.url.replace('{{ tag_name }}', self.tag_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -392,12 +391,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return self.format_item(results)
 
     def getbyapi(self):
         response = None
@@ -418,9 +417,8 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ tag_name }}', self.tag_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -431,12 +429,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return self.format_item(results)
 
     def listbyproduct(self):
         response = None
@@ -456,9 +454,8 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ operation_name }}', self.product_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -469,12 +466,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return [self.format_item(x) for x in results['value']] if results['value'] else []
 
     def listbyapi(self):
         response = None
@@ -494,9 +491,7 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -507,12 +502,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return [self.format_item(x) for x in results['value']] if results['value'] else []
 
     def get(self):
         response = None
@@ -531,9 +526,8 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
+        self.url = self.url.replace('{{ api_name }}', self.api_id)
+        self.url = self.url.replace('{{ tag_name }}', self.tag_id)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -544,12 +538,12 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return self.format_item(results)
 
     def listbyservice(self):
         response = None
@@ -567,9 +561,6 @@ class AzureRMTagInfo(AzureRMModuleBase):
         self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
         self.url = self.url.replace('{{ resource_group }}', self.resource_group)
         self.url = self.url.replace('{{ service_name }}', self.service_name)
-        self.url = self.url.replace('{{ api_name }}', self.api_name)
-        self.url = self.url.replace('{{ operation_name }}', self.operation_name)
-        self.url = self.url.replace('{{ tag_name }}', self.name)
 
         try:
             response = self.mgmt_client.query(self.url,
@@ -580,15 +571,21 @@ class AzureRMTagInfo(AzureRMModuleBase):
                                               self.status_code,
                                               600,
                                               30)
-            results['temp_item'] = json.loads(response.text)
+            results = json.loads(response.text)
             # self.log('Response : {0}'.format(response))
         except CloudError as e:
             self.log('Could not get info for @(Model.ModuleOperationNameUpper).')
 
-        return results
+        return [self.format_item(x) for x in results['value']] if results['value'] else []
 
-    def format_item(item):
-        return item
+    def format_item(self, item):
+        d = {
+            'id': item['id'],
+            'name': item['name'],
+            'type': item['type'],
+            'display_name': item['properties']['displayName']
+        }
+        return d
 
 
 def main():
